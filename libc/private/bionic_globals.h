a8045eecbf0d5dc712f89c129cb1e9590de562a0
Daniel Micay <danielmicay@gmail.com>
2016-01-27 18:02:15 -0500

    add XOR mangling mitigation for thread-local dtors
    memtag_stack struct member is required to be at its exact position by static_assert below.
    
    Signed-off-by: anupritaisno1 <www.anuprita804@gmail.com>

--- libc/private/bionic_globals.h
+++ libc/private/bionic_globals.h
@@ -50,6 +50,7 @@ struct libc_globals {
   uintptr_t heap_pointer_tag;
   _Atomic(bool) memtag_stack;
   _Atomic(bool) decay_time_enabled;
+  long dtor_cookie;
 
   // In order to allow a complete switch between dispatch tables without
   // the need for copying each function by function in the structure,

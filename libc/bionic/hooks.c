90b3f3dde839023e39c6c238865e7cf5d72cf637
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-05-02 16:50:21 +0300

    add hook definitions

---
+++ libc/bionic/hooks.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+
+// android_getaddrinfofornetcontext()
+// SSL_set_tlsext_host_name()
+const char* hook_translate_hostname(const char* hostname) {
+    if (hostname == NULL) {
+        return NULL;
+    }
+
+    switch (get_prog_id()) {
+        default:
+            return hostname;
+    }
+}
+
+// X509_verify_cert()
+const char* hook_get_trusted_ssl_certificate() {
+    switch (get_prog_id()) {
+        default:
+            return NULL;
+    }
+}
+
+// SSL_write()
+void* hook_override_ssl_write(const void* orig_buf, int orig_len, int* out_len) {
+    if (orig_len <= 0) {
+        return NULL;
+    }
+
+    switch (get_prog_id()) {
+        default:
+            return NULL;
+    }
+}

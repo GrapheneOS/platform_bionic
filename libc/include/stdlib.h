705cd653fb6791f768665fd1725a6201df21adb8
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-05-02 16:52:51 +0300

    change behavior of Qualcomm's xtra-daemon

--- libc/include/stdlib.h
+++ libc/include/stdlib.h
@@ -214,6 +214,7 @@ int get_prog_id();
 #define is_prog(id) (get_prog_id() == id)
 
 #define PROG_PIXEL_CAMERA_PROVIDER_SERVICE 1
+#define PROG_XTRA_DAEMON 2
 
 const char* _Nullable hook_translate_hostname(const char* _Nonnull hostname);
 const char* _Nullable const* _Nullable hook_get_trusted_ssl_certificates();

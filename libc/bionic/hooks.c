7e5673884b028b9640c0107e6f2fe26e4cf9b111
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-05-02 16:52:51 +0300

    change behavior of Qualcomm's xtra-daemon

--- libc/bionic/hooks.c
+++ libc/bionic/hooks.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
 
+#include "hooks_xtra_daemon.c"
+
 // android_getaddrinfofornetcontext()
 // SSL_set_tlsext_host_name()
 const char* hook_translate_hostname(const char* hostname) {
@@ -8,6 +10,8 @@ const char* hook_translate_hostname(const char* hostname) {
     }
 
     switch (get_prog_id()) {
+        case PROG_XTRA_DAEMON:
+            return xtra_hook_translate_hostname(hostname);
         default:
             return hostname;
     }
@@ -18,6 +22,8 @@ const char* const* hook_get_trusted_ssl_certificates() {
     // return NULL or NULL-terminated array of base64-encoded SHA-256 sums of public keys of
     // trusted certificates
     switch (get_prog_id()) {
+        case PROG_XTRA_DAEMON:
+            return xtra_hook_get_trusted_ssl_certificates();
         default:
             return NULL;
     }
@@ -30,6 +36,8 @@ void* hook_override_ssl_write(const void* orig_buf, int orig_len, int* out_len) 
     }
 
     switch (get_prog_id()) {
+        case PROG_XTRA_DAEMON:
+            return xtra_hook_override_ssl_write(orig_buf, orig_len, out_len);
         default:
             return NULL;
     }

fdb1bf0344bd410dea231915dd99b7cbf801e4e1
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-08-26 14:43:55 +0300

    add stubs for Pixel Health app
    These stubs allow it to function without the privileged READ_DEVICE_CONFIG permission.

--- framework/java/android/provider/DeviceConfig.java
+++ framework/java/android/provider/DeviceConfig.java
@@ -1249,6 +1249,10 @@ public final class DeviceConfig {
     @Nullable
     public static String getString(@NonNull String namespace, @NonNull String name,
             @Nullable String defaultValue) {
+        if (android.ext.AppInfoExt.getInitialPackageId() == android.ext.PackageId.PIXEL_HEALTH) {
+            return defaultValue;
+        }
+
         String value = getProperty(namespace, name);
         return value != null ? value : defaultValue;
     }
@@ -1272,6 +1276,16 @@ public final class DeviceConfig {
                 // avoid IPC spam.
                 return defaultValue;
             }
+        }
+
+        if (android.ext.AppInfoExt.getInitialPackageId() == android.ext.PackageId.PIXEL_HEALTH) {
+            if ("device_personalization_services".equals(namespace)) {
+                switch (name) {
+                    case "Thermometer__otsv2_enabled":
+                        return true;
+                }
+            }
+            return defaultValue;
         }
 
         String value = getProperty(namespace, name);

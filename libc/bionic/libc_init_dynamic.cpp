ab7baf7dfff051a4af6062638dec14f08ec29622
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-11-15 19:29:03 +0200

    [temporary] disable hardened_malloc for vendor audio service

--- libc/bionic/libc_init_dynamic.cpp
+++ libc/bionic/libc_init_dynamic.cpp
@@ -100,6 +100,10 @@ static void init_prog_id(libc_globals* globals) {
   else if (IS("/system/bin/surfaceflinger")) {
     prog_id = PROG_SURFACEFLINGER;
   }
+  else if (IS("/vendor/bin/hw/android.hardware.audio.service")) {
+    // needed for Pixel Tablet as of Android 15, see https://github.com/GrapheneOS/os-issue-tracker/issues/4306
+    flags = GLOBAL_FLAG_DISABLE_HARDENED_MALLOC;
+  }
 
 #undef IS
 

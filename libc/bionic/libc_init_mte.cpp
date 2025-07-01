98c6c90905a18edeac3311f0d4150502a30a2572
quh4gko8 <88831734+quh4gko8@users.noreply.github.com>
2025-07-01 04:17:52 +0000

    fixup! disable memory tagging for Pixel camera provider service

--- libc/bionic/libc_init_mte.cpp
+++ libc/bionic/libc_init_mte.cpp
@@ -133,7 +133,7 @@ static bool get_environment_memtag_setting(HeapTaggingLevel* level) {
   const bool is_debug_build = is_debuggable_build();
   if (is_vendor_prog) {
     bool apply_override =
-        strcmp(progname, "/apex/com.google.pixel.camera.hal/bin/hw/android.hardware.camera.provider@2.7-service-google")
+        strcmp(progname, "/apex/com.google.pixel.camera.hal/bin/hw/android.hardware.camera.provider@2.7-service-google") != 0
     ;
     if (apply_override) {
         *level = M_HEAP_TAGGING_LEVEL_ASYNC;

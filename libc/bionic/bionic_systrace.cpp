97cc9f4aece01ae22727ec9b7000f9c72f13e63d
Daniel Micay <daniel.micay@grapheneos.org>
2026-09-22 15:19:00 -0400

    Revert "replace VLA formatting with dprintf-like function"
    This reverts commit 982b11b88fc2655b9f471a84b427c50fc53cccb9.
    
    This was unknowingly breaking Bionic trace markers due to using multiple
    writes within async_safe_format_buffer.
    
    Moving away from a VLA is no longer needed to improve safety due to
    having -fstack-clash-protection enabled for quite a while already.

--- libc/bionic/bionic_systrace.cpp
+++ libc/bionic/bionic_systrace.cpp
@@ -29,6 +29,8 @@
 #include <async_safe/log.h>
 #include <cutils/trace.h> // For ATRACE_TAG_BIONIC.
 
+#define WRITE_OFFSET   32
+
 static Lock g_lock;
 static CachedProperty g_debug_atrace_tags_enableflags("debug.atrace.tags.enableflags");
 static uint64_t g_tags;
@@ -59,9 +61,15 @@ static void trace_begin_internal(const char* message) {
     return;
   }
 
+  // If bionic tracing has been enabled, then write the message to the
+  // kernel trace_marker.
+  int length = strlen(message);
+  char buf[length + WRITE_OFFSET];
+  size_t len = async_safe_format_buffer(buf, length + WRITE_OFFSET, "B|%d|%s", getpid(), message);
+
   // Tracing may stop just after checking property and before writing the message.
   // So the write is acceptable to fail. See b/20666100.
-  async_safe_format_fd(trace_marker_fd, "B|%d|%s", getpid(), message);
+  TEMP_FAILURE_RETRY(write(trace_marker_fd, buf, len));
 }
 
 void bionic_trace_begin(const char* message) {
